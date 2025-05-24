b6cd5b4f945af6075ae32af503bcc262a7e2f338
ubuntu <ubuntu@staging.openbadges.education>
2025-05-24 10:16:33 +0200

    handle no errors

--- attachImages.sh
+++ attachImages.sh
@@ -3,15 +3,17 @@
 # Returns the string in the format `, "attachments": [<attachments>]`
 
 result=""
-while IFS= read -r file || [ -n $file ]; do
-	if [ "$result" == "" ]; then
-		result=', "attachments": [';
-	else
-		result="$result, "
-	fi
+while IFS='' read -r file || [ -n "$file" ]; do
+	if [ -n "$file" ]; then
+		if [ "$result" == "" ]; then
+			result=', "attachments": [';
+		else
+			result="$result, "
+		fi
 
-	url=`./uploadImage.sh $file`
-	result="$result{\"text\": \"$file\", \"image_url\": \"$url\"}"
+		url=`./uploadImage.sh $file`
+		result="$result{\"text\": \"$file\", \"image_url\": \"$url\"}"
+	fi
 done < /dev/stdin
 
 [ "$result" != "" ] && result="$result]"
